8a96c0d8157b9382e34fb231b15e66047af2804e
kinish123 <kinishneema7723@gmail.com>
2024-08-02 19:24:56 +0530

    day21.js

---
+++ day21.js
@@ -0,0 +1,133 @@
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+// Test cases
+console.log(twoSum([2, 7, 11, 15], 9));  // Output: [0, 1]
+console.log(twoSum([3, 2, 4], 6));       // Output: [1, 2]
+console.log(twoSum([3, 3], 6));          // Output: [0, 1]
+
+
+function reverseInteger(x) {
+    const reversed = parseInt(x.toString().split('').reverse().join('')) * Math.sign(x);
+    if (reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+    return reversed;
+}
+
+// Test cases
+console.log(reverseInteger(123));   // Output: 321
+console.log(reverseInteger(-123));  // Output: -321
+console.log(reverseInteger(120));   // Output: 21
+
+
+function isPalindrome(x) {
+    if (x < 0) return false;
+    const str = x.toString();
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+// Test cases
+console.log(isPalindrome(121));   // Output: true
+console.log(isPalindrome(-121));  // Output: false
+console.log(isPalindrome(10));    // Output: false
+
+
+function ListNode(val, next = null) {
+    this.val = val;
+    this.next = next;
+}
+
+function mergeTwoLists(l1, l2) {
+    const dummy = new ListNode(-1);
+    let current = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val <= l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    if (l1 !== null) {
+        current.next = l1;
+    } else {
+        current.next = l2;
+    }
+
+    return dummy.next;
+}
+
+// Helper function to create linked list from array
+function createLinkedList(arr) {
+    let dummy = new ListNode(-1);
+    let current = dummy;
+    for (let num of arr) {
+        current.next = new ListNode(num);
+        current = current.next;
+    }
+    return dummy.next;
+}
+
+// Helper function to print linked list
+function printLinkedList(head) {
+    let result = [];
+    while (head !== null) {
+        result.push(head.val);
+        head = head.next;
+    }
+    console.log(result.join(' -> '));
+}
+
+// Test cases
+let l1 = createLinkedList([1, 2, 4]);
+let l2 = createLinkedList([1, 3, 4]);
+let mergedList = mergeTwoLists(l1, l2);
+printLinkedList(mergedList);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+
+
+
+function isValid(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (map[top] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+// Test cases
+console.log(isValid("()"));      // Output: true
+console.log(isValid("()[]{}"));  // Output: true
+console.log(isValid("(]"));      // Output: false
+console.log(isValid("([)]"));    // Output: false
+console.log(isValid("{[]}"));    // Output: true
+
+
