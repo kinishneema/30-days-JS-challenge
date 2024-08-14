977dd51724c9722adb0caa1c736575f62aa272da
kinish123 <kinishneema7723@gmail.com>
2024-08-14 18:17:17 +0530

    few updates

--- day21.js
+++ day21.js
@@ -13,7 +13,6 @@ function twoSum(nums, target) {
 // Test cases
 console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
 console.log(twoSum([3, 2, 4], 6)); // Output: [1, 2]
-console.log(twoSum([3, 3], 6)); // Output: [0, 1]
 
 function reverseInteger(x) {
   const reversed =
